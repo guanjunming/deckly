41231d75e44559d4c274410e3e7e6ca4e39237ef
Guan Junming <guan.junming85@gmail.com>
2024-12-01 23:01:47 +0800

    Add sidebar footer with logout working

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import SideBarContent from "./SideBarContent";
 import { auth } from "@/auth";
-import { Ellipsis } from "lucide-react";
 import logoImage from "../../../public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import SidebarFooter from "./SidebarFooter";
 
 const Sidebar = async () => {
   const session = await auth();
-  const user = session?.user;
 
   return (
     <aside className="sticky top-0 flex h-screen flex-col border-r p-2 lg:min-w-[275px]">
@@ -20,25 +18,7 @@ const Sidebar = async () => {
 
       <SideBarContent />
 
-      <div className="flex w-full items-center gap-2">
-        <button className="flex w-full items-center gap-3 overflow-hidden rounded-full p-3 hover:bg-zinc-200">
-          <Avatar className="flex-shrink-0">
-            <AvatarImage src={user?.image ? user.image : ""} />
-            <AvatarFallback className="uppercase">
-              {user?.name ? user.name[0] : ""}
-            </AvatarFallback>
-          </Avatar>
-          <div className="hidden flex-1 flex-col overflow-hidden text-left lg:flex">
-            <div className="font-bold">{user ? user.name : "Unknown"}</div>
-            <div className="text-sm text-muted-foreground">
-              {user ? user.email : "Unknown"}
-            </div>
-          </div>
-          <div className="hidden flex-shrink-0 lg:block">
-            <Ellipsis size={18.75} />
-          </div>
-        </button>
-      </div>
+      <SidebarFooter session={session} />
     </aside>
   );
 };
---
+++ src/components/sidebar/SidebarFooter.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Session } from "next-auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Ellipsis } from "lucide-react";
+import { logout } from "@/actions/auth";
+import Link from "next/link";
+
+interface DropdownMenuProps {
+  session: Session | null;
+}
+
+const SidebarFooter = ({ session }: DropdownMenuProps) => {
+  const user = session?.user;
+
+  return (
+    <div className="flex w-full items-center gap-2">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button className="flex w-full items-center gap-3 overflow-hidden rounded-full p-3 hover:bg-zinc-200">
+            <Avatar className="flex-shrink-0">
+              <AvatarImage
+                src={user?.image ? user.image : ""}
+                alt={user?.name ? user.name : ""}
+              />
+              <AvatarFallback className="uppercase">
+                {user?.name ? user.name[0] : "NK"}
+              </AvatarFallback>
+            </Avatar>
+            <div className="hidden flex-1 flex-col overflow-hidden text-left lg:flex">
+              <div className="font-bold">{user ? user.name : "Unknown"}</div>
+              <div className="text-sm text-muted-foreground">
+                {user ? user.email : "Unknown"}
+              </div>
+            </div>
+            <div className="hidden flex-shrink-0 lg:block">
+              <Ellipsis size={18} />
+            </div>
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-60">
+          <DropdownMenuItem asChild className="cursor-pointer text-base">
+            <Link href="/settings">Settings</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer text-base"
+            onClick={() => logout()}
+          >
+            <span>Log out</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default SidebarFooter;
